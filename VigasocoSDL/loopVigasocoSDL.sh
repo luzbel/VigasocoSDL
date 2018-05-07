878ed86a5ac614e23c841ede3f99a9c0fa84cffe
bazarum <juantomas.garcia@gmail.com>
2018-05-07 16:12:35 +0200

    create a debugging log to check why and when the game engine cracks sometime

--- VigasocoSDL/loopVigasocoSDL.sh
+++ VigasocoSDL/loopVigasocoSDL.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true
 do
-  nice -15 ./VigasocoSDL abadia -input:libVigasocoFakeInputPlugin.so,FakeInputPlugin  -audio:libVigasocoNULLAudioPlugin.so,NULLAudioPlugin
+  nice -15 ./VigasocoSDL abadia -input:libVigasocoFakeInputPlugin.so,FakeInputPlugin  -audio:libVigasocoNULLAudioPlugin.so,NULLAudioPlugin | tee -a /tmp/viga.log
+  tail -20 /tmp/viga.log > /tmp/VigasocoSDK.log
 done
