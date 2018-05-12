ababeb923df80ca4c61182482999cf57845e05bd
bazarum <juantomas.garcia@gmail.com>
2018-05-12 23:44:55 +0200

    improving the logs for debugging

--- VigasocoSDL/loopVigasocoSDL.sh
+++ VigasocoSDL/loopVigasocoSDL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-  nice -15 ./VigasocoSDL abadia -input:libVigasocoFakeInputPlugin.so,FakeInputPlugin  -audio:libVigasocoNULLAudioPlugin.so,NULLAudioPlugin | tee -a /tmp/viga.log
-  tail -20 /tmp/viga.log > /tmp/VigasocoSDK.log
+  nice -15 ./VigasocoSDL abadia -input:libVigasocoFakeInputPlugin.so,FakeInputPlugin  -audio:libVigasocoNULLAudioPlugin.so,NULLAudioPlugin 2>&1 | tee -a /tmp/viga.log
+  tail -40 /tmp/viga.log >> /tmp/VigasocoSDK.log
 done
